76bcbe414ddae6019fd9c2cf7fefad79b62764c4
emirhanaricii <emirhan190341@gmail.com>
2023-10-08 22:24:41 +0300

    post page added

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,26 +1,95 @@
-import { ListItem, Text, UnorderedList } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { BsThreeDots } from "react-icons/bs";
+import Actions from "../components/Actions";
+import Comment from "../components/Comment";
 
 const PostPage = () => {
-  const [postList, setPostList] = useState([]);
-
-  useEffect(() => {
-    async function getPostData() {
-      const response = await fetch("http://localhost:8080/v1/api/post");
-      const body = await response.json();
-      setPostList(body);
-      console.log(body);
-    }
-    getPostData();
-  }, []);
+  const [liked, setLiked] = useState(false);
 
   return (
     <>
-      <UnorderedList>
-        {postList.map((post) => (
-          <ListItem>{post.title} {post.text}</ListItem>
-        ))}
-      </UnorderedList>
+      <Flex>
+        <Flex w={"full"} alignItems={"center"} gap={3}>
+          <Avatar src="mark.jpeg" size={"md"} name="Mark Zuckerberg" />
+          <Flex>
+            <Text fontSize={"sm"} fontWeight={"bold"}>
+              markzuckerberg
+            </Text>
+            <Image src="verified.png" w={4} ml={4} h={4} />
+          </Flex>
+        </Flex>
+        <Flex gap={4} alignItems={"center"}>
+          <Text fontSize={"sm"} color={"gray.light"}>
+            1d
+          </Text>
+          <BsThreeDots />
+        </Flex>
+      </Flex>
+
+      <Text my={3}>Let's talk about Threads.</Text>
+
+      <Box
+        borderRadius={6}
+        overflow={"hidden"}
+        border={"1px solid"}
+        borderColor={"gray.light"}
+      >
+        <Image src="post1.jpg" />
+      </Box>
+
+      <Flex gap={3} my={3}>
+        <Actions liked={liked} setLiked={setLiked} />
+      </Flex>
+
+      <Flex gap={2} alignItems={"center"}>
+        <Text color={"gray.light"} fontSize={"sm"}>
+          238 replies
+        </Text>
+        <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+        <Text color={"gray.light"} fontSize={"sm"}>
+          {200 + (liked ? 1 : 0)} likes
+        </Text>
+      </Flex>
+      <Divider my={5} />
+
+      <Flex gap={5} justifyContent={"space-between"} alignItems={"center"}>
+        <Flex gap={3} alignItems={"center"}>
+          <Text fontSize={"30"}>👋</Text>
+          <Text color={"gray.light"}>Get the app to like, reply and post.</Text>
+        </Flex>
+        <Button>Get</Button>
+      </Flex>
+      <Divider my={5} />
+      <Comment
+        comment="Looks really good!"
+        createdAt="2d"
+        likes={100}
+        username="john doe"
+        userAvatar="https://bit.ly/prosper-baba"
+      />
+      <Comment
+        comment="Looks really good!"
+        createdAt="3d"
+        likes={100}
+        username="jane doe"
+        userAvatar="https://bit.ly/prosper-baba"
+      />
+      <Comment
+        comment="aasdasdasg!"
+        createdAt="2d"
+        likes={100}
+        username="emirhan arici"
+        userAvatar="https://bit.ly/prosper-baba"
+      />
     </>
   );
 };
